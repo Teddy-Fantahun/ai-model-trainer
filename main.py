c1041e047aa947fb8c73a3e279a44573a354763f
Tewodros Fantahun <tewodrosfantahun2011@gmail.com>
2024-10-26 22:55:50 +0300

    Added the main script

---
+++ main.py
@@ -0,0 +1,44 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from transformers import TrainingArguments
+from transformers import TrainingArguments, Trainer
+import numpy as np
+import evaluate
+from huggingface_hub import login
+
+login()
+
+dataset = load_dataset("yelp_review_full")
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+
+model = AutoModelForSequenceClassification.from_pretrained("meta-llama/Llama-2-7b-chat-hf", num_labels=5)
+
+training_args = TrainingArguments(output_dir="test_trainer")
+
+training_args = TrainingArguments(output_dir="test_trainer", eval_strategy="epoch")
+
+metric = evaluate.load("accuracy")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=small_train_dataset,
+    eval_dataset=small_eval_dataset,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
